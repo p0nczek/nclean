69b58b2caf0a84e2ad799a04e6794e46434a2ca4
shin <76970908+p0nczek@users.noreply.github.com>
2026-07-09 23:50:56 +0200

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "nix-sweeper: curses TUI for managing NixOS generations";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "nclean";
+            version = "1.0.0";
+
+            dontUnpack = true;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            installPhase = ''
+              install -Dm755 ${./nclean.py} $out/bin/.nix-sweeper-unwrapped
+              makeWrapper ${pkgs.python3}/bin/python3 $out/bin/nix-sweeper \
+                --add-flags $out/bin/.nix-sweeper-unwrapped \
+                --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.nh ]}
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Curses TUI for managing NixOS generations";
+              license = licenses.mit;
+              platforms = platforms.linux;
+              mainProgram = "nclean";
+            };
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/nclean";
+        };
+      });
+    };
+}
